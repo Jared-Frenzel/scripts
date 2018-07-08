c4fb8dda0acaf5a9e8515fac450b63855c095bac
Jared Frenzel <jared.frenzel.77@gmail.com>
2018-07-08 16:23:18 -0500

    Added multiplatform support

--- wallpaper_boi.py
+++ wallpaper_boi.py
@@ -7,9 +7,26 @@ import urllib.request
 import numpy as np
 import string 
 import os
+import platform
+
+
+set_wallpaper = {
+    'Windows': lambda new_wallpaper_path: exit(),
+    'Linux': lambda new_wallpaper_path: os.system(f'gsettings set org.gnome.desktop.background picture-uri file://{new_wallpaper_path}'),
+    'Darwin': lambda new_wallpaper_path: os.system(f'osascript -e \'tell application "Finder" to set desktop picture to POSIX file"{new_wallpaper_path}"\'')
+}
+
+config_path = {
+    'Linux': '~/.wallpaper_boi',
+    'Darwin': '~/.wallpaper_boi'
+}
 
 # Load config from ~/.wallpaper_boi
-with open(os.path.expanduser('~/.wallpaper_boi'),'r') as config:
+if platform.system() not in config_path.keys():
+    print(f'{platform.system()} is not currently supported')
+    exit()
+
+with open(os.path.expanduser(config_path[platform.system()]),'r') as config:
 	configs = { line.split(':')[0]:line.split(':')[1] for line in config.read().split('\n') if ':' in line }
 
 image_types = configs['supported_image_types'].split(',')
@@ -36,7 +53,8 @@ reddit_url = f"http://www.reddit.com/r/{target_subreddit}/"
 result = re.finditer(image_regex, str(urllib.request.urlopen(reddit_url).read()))
 
 urls = [url.group(0) for url in result]
-
+print(urls)
+new_wallpaper_path = ""
 
 # Try downloading the top rated links, save to configured folder
 for URL in urls:
@@ -44,8 +62,13 @@ for URL in urls:
 		with urllib.request.urlopen(URL) as url:
 			random_name = "".join(np.random.choice(list(string.ascii_lowercase), 10))
 			image_type = URL.split(".")[-1]
-			with open(f'{wallpaper_folder}/{random_name}.{image_type}', 'wb') as f:
+			new_wallpaper_path = f'{wallpaper_folder}/{random_name}.{image_type}'
+			with open(new_wallpaper_path, 'wb') as f:
 				f.write(url.read())
+		print(f'Attempting w/ {URL}')
 		break
 	except Exception as e:
 		print(e)
+
+# Set the wallpaper
+set_wallpaper[platform.system()](new_wallpaper_path)
